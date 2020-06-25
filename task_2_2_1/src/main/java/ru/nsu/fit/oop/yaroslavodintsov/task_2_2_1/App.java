7edd99ed2d9b64d6e0f1634830f44ac00fcec36c
YYYYYYYYYYYYYYYYYYYYY <45619622+YYYYYYYYYYYYYYYYYYYYY@users.noreply.github.com>
2020-06-26 03:55:43 +0700

    Add files via upload

---
+++ task_2_2_1/src/main/java/ru/nsu/fit/oop/yaroslavodintsov/task_2_2_1/App.java
@@ -0,0 +1,11 @@
+package ru.nsu.fit.oop.yaroslavodintsov.task_2_2_1;
+
+import javafx.application.Application;
+
+@SuppressWarnings("restriction")
+public class App {
+
+    public static void main(String[] args) {
+        Application.launch(SnakeGameGUI.class, args);
+    }
+}
---
+++ task_2_2_1/src/main/java/ru/nsu/fit/oop/yaroslavodintsov/task_2_2_1/CellStatus.java
@@ -0,0 +1,14 @@
+package ru.nsu.fit.oop.yaroslavodintsov.task_2_2_1;
+
+import ru.nsu.fit.oop.yaroslavodintsov.task_2_2_1.CellButton;
+
+/**
+ * The CellStatus enum represents the "element" that a {@link CellButton}
+ * contains.
+ *
+ */
+public enum CellStatus {
+
+    SNAKE, BOARD, FRUIT
+
+}
